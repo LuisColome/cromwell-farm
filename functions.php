52a46a81fad6f55dab2b8f254e826c2b7fc08264
Luis Colomé <hello@luiscolome.com>
2021-07-17 18:06:14 +0200

    Rounded buttons + mobile menu
    Removed the movile nav header and added a new animation to the
    hamburger menu.
    Basic typography styles added.

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ add_action( 'enqueue_block_editor_assets', 'ea_gutenberg_scripts' );
 *
 */
 function lcm_theme_fonts_url() {
-	return 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap';
+	return 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400&display=swap';
 }
 
 function ea_child_theme_setup() {

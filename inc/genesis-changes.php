62c24bd76e3cef2d3def65eb3105189fcf76d781
Luis Colomé <hello@luiscolome.com>
2021-07-18 13:09:19 +0200

    Fix image size on single posts

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -180,6 +180,6 @@ add_filter( 'body_class', 'genesis_sample_blocks_body_classes' );
 function featured_post_image() {
     if ( ! is_singular( 'post' ) )
       return;
-      the_post_thumbnail('cfhh-featured-images');
+      the_post_thumbnail('cf-featured-image');
   }
   add_action( 'genesis_entry_content', 'featured_post_image', 8 );
